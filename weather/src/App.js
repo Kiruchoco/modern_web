248a74120791e7231fe4e93ccb788635bf8f5d86
Kiruchoco <sba1013kr@naver.com>
2024-02-08 15:52:36 +0900

    weather complete

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [temp, setTemp] = useState("");
+  const [desc, setDesc] = useState("");
+  const [icon, setIcon] = useState("");
+  const [isReady, setReady] = useState(false);
+
+  React.useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=[API 키]&units=metric"
+    )
+      .then((result) => result.json())
+      .then((jsonresult) => {
+        setTemp(jsonresult.main.temp);
+        setDesc(jsonresult.weather[0].main);
+        setIcon(jsonresult.weather[0].icon);
+        setReady(true);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (isReady) {
+    return (
+      <div className="App">
+        <p>Temperature: {temp} </p>
+        <p>Description: {desc}</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt="Weather icon"
+        />
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default App;
